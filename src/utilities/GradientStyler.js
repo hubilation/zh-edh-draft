b6ab1e755850893c114c2f4cbb9ed8b78f262d4b
zack.huber <zack.huber@gmail.com>
2017-08-06 09:04:12 -0700

    added gradient styler

---
+++ src/utilities/GradientStyler.js
@@ -0,0 +1,57 @@
+import Constants from "../constants";
+
+export default class GradientStyler {
+  static getGradientStyle(colors) {
+    var gradientProps = [];
+
+    gradientProps.push("to right");
+
+    if (!colors) gradientProps.push("#D7D1D0");
+    else {
+      var hexCodes = colors.map(function(color) {
+        var loweredColor = color.toLowerCase();
+        return Constants.Colors[loweredColor];
+      });
+
+      for (var i = 0; i < hexCodes.length; i++) {
+        var code = hexCodes[i];
+
+        gradientProps.push(code);
+      }
+    }
+    if (!colors || colors.length < 2) {
+      gradientProps.push("white");
+    }
+    var gradient = "linear-gradient(" + gradientProps.join(" ,") + ")";
+    return {
+      background: gradient
+    };
+  }
+
+  static getGradientStyleReversed(colors) {
+    var gradientProps = [];
+
+    gradientProps.push("to right");
+
+    if (!colors || colors.length < 2) {
+      gradientProps.push("white");
+    }
+    if (!colors) gradientProps.push("#D7D1D0");
+    else {
+      var hexCodes = colors.map(function(color) {
+        var loweredColor = color.toLowerCase();
+        return Constants.Colors[loweredColor];
+      });
+
+      for (var i = 0; i < hexCodes.length; i++) {
+        var code = hexCodes[i];
+
+        gradientProps.push(code);
+      }
+    }
+    var gradient = "linear-gradient(" + gradientProps.join(" ,") + ")";
+    return {
+      background: gradient
+    };
+  }
+}
